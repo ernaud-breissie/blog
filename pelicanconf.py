5a3d0cce27552f793b66fd97875abf6f625fec2e
ernaud-breissie <ernaudbreissie@gmail.com>
2024-01-24 21:35:57 +0100

    wip main Updated: 2024-01-24 21:35:57

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,12 +11,12 @@ DEFAULT_LANG = 'en'
 THEME = 'notmyidea'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+FEED_MAX_ITEMS = 15
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 # Blogroll
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
